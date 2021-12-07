ba140e27ba06567f532a11718cfd87646e483e1c
Youssef Henna <youssef.hisham14@gmail.com>
2021-12-07 20:15:39 +0200

    Finalized

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
-const { json } = require("express");
 require("dotenv").config();
 
 app.set("view engine", "ejs");
@@ -40,11 +39,62 @@ client.connect((err) => {
   });
 
   app.get("/login", function (req, res) {
-    res.render("pages/login", { title: "Hello there" });
+    res.render("pages/login");
   });
 
   app.get("/register", function (req, res) {
-    res.render("pages/register", { title: "Hello there" });
+    res.render("pages/register");
+  });
+
+  app.get("/forgot_password", function (req, res) {
+    res.render("pages/forgot_password");
+  });
+
+  app.get("/question_reset", async function (req, res) {
+    const username = req.query.username;
+    if (!username) {
+      return res.status(400).send("Missing fields");
+    }
+    const existing = await db.collection("users").findOne({ username });
+    if (!existing) {
+      return res.status(400).send("User does not exist");
+    }
+    res.render("pages/question_reset", {
+      question: existing.security_question,
+      username,
+    });
+  });
+
+  app.get("/home", async function (req, res) {
+    const search = req.query.search;
+    let shows;
+    if (search) {
+      db.collection("shows").createIndex({ name: "text" });
+      shows = await db
+        .collection("shows")
+        .find({ $text: { $search: search } })
+        .toArray();
+    } else {
+      shows = await db.collection("shows").find().toArray();
+    }
+    res.render("pages/home", {
+      shows,
+      search,
+    });
+  });
+
+  app.get("/blog", async function (req, res) {
+    const id = req.query.id;
+    if (!id) {
+      return res.status(400).send("Missing fields");
+    }
+    const result = await db.collection("shows").findOne({ _id: ObjectId(id) });
+    if (!result) {
+      return res.status(400).send("Show does not exist");
+    }
+    res.render("pages/blog", {
+      show: result,
+    });
   });
 
   app.post("/auth/login", async function (req, res) {
@@ -52,24 +102,28 @@ client.connect((err) => {
       const { username, password } = req.body;
 
       if (!username || !password) {
-        res.status(400).send("Missing fields");
+        return res.status(400).send("Missing fields");
       }
 
       const existing = await db.collection("users").findOne({ username });
       if (!existing) {
         return res.status(400).send("User does not exist");
       }
-      if (password == existing.password) {
-        const token = jwt.sign({ id: existing._id }, process.env.TOKEN_KEY, {
-          expiresIn: "7d",
-        });
+      if (password === existing.password) {
+        const token = jwt.sign(
+          { id: existing._id, username },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: "7d",
+          }
+        );
         const date = new Date();
         date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000);
 
         res.cookie("token", token, { expires: date });
         res.redirect("/");
       } else {
-        res.status(401);
+        res.status(401).send("Incorrect password");
       }
     } catch (e) {
       console.log(e);
@@ -79,10 +133,11 @@ client.connect((err) => {
 
   app.post("/auth/register", async function (req, res) {
     try {
-      const { username, password } = req.body;
+      const { username, password, security_question, security_answer } =
+        req.body;
 
-      if (!username || !password) {
-        res.status(400).send("Missing fields");
+      if (!username || !password || !security_question || !security_answer) {
+        return res.status(400).send("Missing fields");
       }
 
       const existing = await db.collection("users").findOne({ username });
@@ -91,14 +146,108 @@ client.connect((err) => {
       }
       const result = await db
         .collection("users")
-        .insertOne({ username, password });
-      const token = jwt.sign({ id: result.insertedId }, process.env.TOKEN_KEY, {
-        expiresIn: "7d",
-      });
+        .insertOne({ username, password, security_question, security_answer });
+      const token = jwt.sign(
+        { id: result.insertedId, username },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "7d",
+        }
+      );
       const date = new Date();
       date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000);
 
       res.cookie("token", token, { expires: date });
+      res.redirect("/");
+    } catch (e) {
+      console.log(e);
+      res.status(500).send();
+    }
+  });
+
+  app.post("/auth/reset_password", async function (req, res) {
+    try {
+      const { username, password, security_answer } = req.body;
+
+      if (!username || !password || !security_answer) {
+        return res.status(400).send("Missing fields");
+      }
+
+      const existing = await db.collection("users").findOne({ username });
+      if (!existing) {
+        return res.status(400).send("User does not exist");
+      }
+      if (existing.security_answer === security_answer) {
+        await db.collection("users").updateOne(
+          {
+            username,
+          },
+          { $set: { password } }
+        );
+        res.redirect("/login");
+      } else {
+        return res.status(401).send("Incorrect answer");
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).send();
+    }
+  });
+
+  app.post("/add_show", async function (req, res) {
+    try {
+      const { name, overview, poster_path } = req.body;
+
+      if (!name || !overview || !poster_path) {
+        return res.status(400).send("Missing fields");
+      }
+      await db.collection("shows").insertOne({ name, overview, poster_path });
+      res.redirect("/home");
+    } catch (e) {
+      console.log(e);
+      res.status(500).send();
+    }
+  });
+
+  app.post("/add_comment", async function (req, res) {
+    try {
+      const { comment, show_id } = req.body;
+
+      if (!comment || !show_id) {
+        return res.status(400).send("Missing fields");
+      }
+      const result = await db
+        .collection("shows")
+        .findOne({ _id: ObjectId(show_id) });
+      if (!result) {
+        return res.status(400).send("Show not found");
+      }
+      jwt.verify(
+        req.cookies.token,
+        process.env.TOKEN_KEY,
+        async function (err, decoded) {
+          if (err) {
+            return res.status(401).send("Not authenticated");
+          } else {
+            await db.collection("shows").updateOne(
+              { _id: ObjectId(show_id) },
+              {
+                $push: { comments: { commenter: decoded.username, comment } },
+              }
+            );
+            res.redirect("/blog?id=" + show_id);
+          }
+        }
+      );
+    } catch (e) {
+      console.log(e);
+      res.status(500).send();
+    }
+  });
+
+  app.post("/sign_out", async function (req, res) {
+    try {
+      res.clearCookie("token");
       res.redirect("/");
     } catch (e) {
       console.log(e);
